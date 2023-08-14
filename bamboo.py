ad7b7e111986e2f886ffcc5f613345c2a51c2c97
gurelkin <gurshalevelkin@gmail.com>
2023-08-14 23:29:42 +0300

    Rotating intact segmentation images correctly

--- bamboo.py
+++ bamboo.py
@@ -2,6 +2,9 @@ from json import dump
 from os import listdir
 from sys import argv
 
+import numpy as np
+from skimage import io, transform
+
 from consts import *
 from find import *
 
@@ -34,5 +37,45 @@ def my_main():
         frag.plot()
 
 
+def angles_poll(angles):
+    real_angles = [a for a in angles if a is not None]
+    if len(real_angles) > 0:
+        return np.mean(real_angles)
+    return None
+
+    # Previous attempt:
+    # real_angles = [a for a in angles if a is not None]
+    # if len(real_angles) == 0:
+    #     return None
+    # angle_votes = np.zeros_like(real_angles)
+    # for index_1, angle_1 in enumerate(real_angles):
+    #     for index2, angle_2 in enumerate(real_angles):
+    #         if index_1 != index2 and abs(angle_1 - angle_2) < epsilon:
+    #             angle_votes[index_1] += 1
+    # print(angle_votes)
+    # return real_angles[np.argmax(angle_votes)]
+
+
+def align_intact(frag_path, intact_path, show=False):
+    frag = Render(frag_path, pose='backside')
+    if show:
+        frag.plot()
+    angles = [find_bamboo_lines_angle(frag, show=show) for i in range(10)]
+    angle = angles_poll(angles)
+    if angle is not None:
+        intact = io.imread(intact_path)
+        rotated = transform.rotate(intact, np.rad2deg(angle))
+        if show:
+            io.imshow(aligned)
+        return rotated
+    return None
+
+
 if __name__ == '__main__':
-    my_main()
+    for frag_id in ["09"]+([str(i) for i in range(10, 21)]):
+        frag_path = "data/group39/RPf_003"+frag_id+".obj"
+        intact_path = "data/group39/RPf_003"+frag_id+"_intact_mesh.png"
+        aligned = align_intact(frag_path, intact_path)
+        if aligned is not None:
+            aligned = np.array(aligned * 255, dtype="uint8")
+            io.imsave("out/RPf_003"+frag_id+"_aligned.png", aligned)
