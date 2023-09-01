f763c7bfee534206c9e75bde14ce5586f2c70c3f
gurelkin <gurshalevelkin@gmail.com>
2023-09-01 16:54:53 +0300

    Added functionality to work with multiple data directories

--- bamboo.py
+++ bamboo.py
@@ -1,27 +1,40 @@
-from json import dump
-from os import listdir
+import os
 from sys import argv
-
-import numpy as np
+from json import dump
 from skimage import io, transform
 
-from consts import *
 from find import *
+
+
+def angles_poll(angles):
+    real_angles = [a for a in angles if a is not None]
+    if len(real_angles) > 0:
+        return np.mean(real_angles)
+    return None
 
 
 def main():
     # bamboo.py <data_path> <out_path> <show>
-    data_path = argv[1]  # path to the data directory
-    out_path = argv[2]   # path to the output directory
+    data = argv[1]  # path to the data directory
+    out = argv[2]   # path to the output directory
     show = len(argv) == 4 and argv[3] == "-s"  # plots the found bamboo lines
-    filenames = [fn for fn in listdir(data_path) if fn.endswith(OBJ)]
-    for fn in filenames:
-        data_file_path = data_path + "/" + fn
-        fragment = Render(data_file_path)
-        bamboo_angle = find_bamboo_lines_angle(fragment, show)
-        out_file_path = out_path + "/" + fn.removesuffix(OBJ) + JSON
-        with open(out_file_path, mode='w') as out_file:
-            dump(bamboo_angle, out_file)
+    group_dirs = [gd for gd in os.listdir(data) if gd.startswith("group")]
+    for group in group_dirs:
+        print("---" + group + "---")
+        group_data = os.path.join(data, group)
+        group_out = os.path.join(out, group)
+        if not os.path.exists(group_out):
+            os.mkdir(group_out)
+        file_names = [fn for fn in os.listdir(group_data) if fn.endswith(OBJ)]
+        for fn in file_names:
+            print(fn)
+            data_file_path = os.path.join(group_data, fn)
+            fragment = Render(data_file_path, pose='backside')
+            bamboo_angles = [find_bamboo_lines_angle(fragment, show) for i in range(10)]
+            chosen_angle = angles_poll(bamboo_angles)
+            out_file_path = os.path.join(group_out, fn.removesuffix(OBJ) + JSON)
+            with open(out_file_path, mode='w') as out_file:
+                dump(chosen_angle, out_file)
 
 
 def my_main():
@@ -36,12 +49,6 @@ def my_main():
         frag.planar_rotation(angle)
         frag.plot()
 
-
-def angles_poll(angles):
-    real_angles = [a for a in angles if a is not None]
-    if len(real_angles) > 0:
-        return np.mean(real_angles)
-    return None
 
     # Previous attempt:
     # real_angles = [a for a in angles if a is not None]
@@ -66,12 +73,12 @@ def align_intact(frag_path, intact_path, show=False):
         intact = io.imread(intact_path)
         rotated = transform.rotate(intact, np.rad2deg(angle))
         if show:
-            io.imshow(aligned)
+            io.imshow(rotated)
         return rotated
     return None
 
 
-if __name__ == '__main__':
+def my_main_2():
     for frag_id in ["09"]+([str(i) for i in range(10, 21)]):
         frag_path = "data/group39/RPf_003"+frag_id+".obj"
         intact_path = "data/group39/RPf_003"+frag_id+"_intact_mesh.png"
@@ -79,3 +86,7 @@ if __name__ == '__main__':
         if aligned is not None:
             aligned = np.array(aligned * 255, dtype="uint8")
             io.imsave("out/RPf_003"+frag_id+"_aligned.png", aligned)
+
+
+if __name__ == '__main__':
+    main()
